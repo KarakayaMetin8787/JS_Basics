1dcdb4f7df67828be9ea2f5c044772459a87da35
KarakayaMetin8787 <karakayametin8787@gmail.com>
2023-10-16 15:16:05 +0200

    Übungen zu JS Basics

---
+++ assets/js/main.js
@@ -0,0 +1,163 @@
+// Intro-Level-1_3
+console.log("%c Intro-Level-1_3", "color: red");
+
+let i = 1;
+console.log(typeof i);
+let lastName = "Johnson";
+console.log(typeof lastName);
+let j = "2";
+console.log(typeof j);
+let status = true;
+console.log(typeof status);
+let hello = "hello";
+console.log(typeof hello);
+let helloWorld = "hello";
+console.log(typeof helloWorld);
+
+// Intro-Level-1_4
+console.log("%c Intro-Level-1_4", "color: red");
+
+console.log(typeof "John");
+console.log(typeof 3.14);
+console.log(typeof NaN);
+console.log(typeof false);
+console.log(typeof [1,2,3,4]);
+console.log(typeof {name:"john", age:34});
+console.log(typeof new Date());
+console.log(typeof function (){});
+console.log(typeof null);
+console.log(typeof 3+2==5);
+console.log(typeof 3 + "3");
+
+// Intro-Level-1_7
+console.log("%c Intro-Level-1_7", "color: red");
+
+const helloworld = "Hello World";
+const pi = 3.14;
+const burjdubai = "828m";
+const fullname = "Jan Schmidt";
+const eifeltower = "324m"
+const camelcase = "camelCase"
+const kursstatus = true;
+
+// Variablen-Level-1_5
+console.log("%c Variablen-Level-1_5", "color: red");
+
+// const carName = "BMW";
+// console.log(carName);
+// const x = 150;
+// console.log(x);
+// const y = 50;
+// console.log(y);
+// const z = x + y;
+// console.log(z);
+
+let firstName = "John";
+let lastName2 = "Doe"; // vorherige Aufgabe hat bereits diese Variable.
+let age = 35;
+
+console.log(firstName);
+console.log(lastName2);
+console.log(age);
+
+// Variablen-Level-1_6
+console.log("%c Variablen-Level-1_6", "color: red");
+
+// let x = 34;
+// let x = 67;
+// console.log(x);
+
+// const y = 34;
+// const y = 67;
+// console.log(y); // const kann nicht von 34 auf 67 umdeklariert werden, da es eine Konstante ist
+
+// Intro-Level-1_8
+console.log("%c Intro-Level-1_8", "color: red");
+
+// const x = 20;
+// const y = 30;
+// console.log(x + y);
+// console.log(y - x);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// const z = 10;
+// const resultOne = x * y / z;
+// let a = 15;
+// let b = 9;
+// console.log(a % b);
+// const c = 20;
+// const resultTwo = c * (a + b);
+// console.log(resultTwo);
+// console.log(a++);
+// console.log(b--);
+// const resultThree = b - a;
+// console.log(resultOne % resultTwo);
+
+// Intro-Level-1_12
+console.log("%c Intro-Level-1_12", "color: red");
+
+// let score = 5 + 5 * 10;
+// console.log('Ergebnis: ' + score);
+
+// let score = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score);
+
+// let score = 0;
+// score = score + 10;
+// console.log('Ergebnis: ' + score);
+
+// score += 10;
+// console.log('Ergebnis: ' + score);
+
+// let score = 2;
+// score++;
+// console.log('Ergebnis: ' + score);
+
+// Intro-Level-1_13
+console.log("%c Intro-Level-1_13", "color: red");
+
+// Addition
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+ // Subtraktion
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+// Multiplikation
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+// Division
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+// Modul: zeigt an, was der Rest sein würde.
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
+
+// String-Methods-Level-1_11
+console.log("%c String-Methods-Level-1_11", "color: red");
+
+let x = "Hello";
+let y = "World";
+let z = x + y;
+document.write(z + "<br>");
+let n = x + " " + y;
+document.write(n + "<br>");
+document.write(x + " World" + "<br>");
+let meinString = "Ich bin Erste:r";
+console.log(meinString + " Ich komme auf Platz zwei");
+
+// Output-Level-1_4
+console.log("%c Output-Level-1_4", "color: red");
+
+window.alert("Hallo Welt");
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+// let alter = window.prompt( "Gib mir bitte dein Alter:" );
+// console.log(alter);
+// let b = 5;
+// let a = b * 5 -3;
+// window.alert(a);
+// let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+// let grosse = "2,72m";
+// window.confirm(mann + grosse);
